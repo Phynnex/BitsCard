492bfc2b9270c132404e68fe2ad26edfb8875be8
ekeoma1 <ekeigwee@gmail.com>
2022-07-01 05:58:17 +0100

    update on logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../assets/image/logo.svg";
+import Logo from "../assets/image/logo.png";
 
 const Navbar = () => {
   return (
